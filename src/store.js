57cf53db23a32f21b086898376a135c62ca8b14c
Taimoor Aslam <taymooraslam@gmail.com>
2017-09-04 18:10:32 +0500

    Apply redux-logger middleware to debug states during developement

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ import logger from 'redux-logger';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(thunk);
+const middleware = applyMiddleware(thunk, logger);
 
 export default createStore(reducer, middleware);
